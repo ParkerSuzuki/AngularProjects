e257a7dbda03d05c092c0332f47739de70273c1a
Parker Suzuki <suzukiparker5@gmail.com>
2026-08-10 11:23:55 -0600

    CH02: Update title for Chapter 2

--- my-app/src/app/chapter.ts
+++ my-app/src/app/chapter.ts
@@ -2,5 +2,5 @@ import { Injectable, signal } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class ChapterService {
-  readonly chapterTitle = signal('Chapter 1: Angular AI Kick-Starter');
+  readonly chapterTitle = signal('Chapter 2: IssueTracker');
 }
