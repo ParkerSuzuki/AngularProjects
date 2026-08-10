ad8b9318e8280a55ab04cfb2cedb1f15a228a9b2
Parker Suzuki <suzukiparker5@gmail.com>
2026-08-10 11:38:01 -0600

    CH02: Implement issue service and add mock issues

--- my-app/src/app/issues.ts
+++ my-app/src/app/issues.ts
@@ -1,4 +1,8 @@
 import { Service } from '@angular/core';
+import { Issue } from './issue';
+import { issueData } from '../../public/mock-issues';
 
 @Service()
-export class Issues {}
+export class Issues {
+  readonly issues = signal<Issue[]>([]);
+}
