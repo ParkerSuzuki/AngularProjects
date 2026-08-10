18b10159ab2f6b0699e70b136e5ea51ae712d5ed
Parker Suzuki <suzukiparker5@gmail.com>
2026-08-10 11:48:01 -0600

    CH02: Add new issue list to main app component

--- my-app/src/app/app.ts
+++ my-app/src/app/app.ts
@@ -3,10 +3,16 @@ import { RouterOutlet } from '@angular/router';
 import { ChapterTitle } from './chapter-title/chapter-title.componenet';
 import { ChapterService } from './chapter';
 import { ClrLayoutModule } from '@clr/angular';
+import { IssueList } from './issue-list/issue-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ChapterTitle, ClrLayoutModule],
+  imports: [
+    RouterOutlet,
+    ChapterTitle,
+    ClrLayoutModule,
+    IssueList
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
