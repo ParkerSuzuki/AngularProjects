9b0b9414f4c48fedabfbdb5d2d521f2c6874e7a6
Parker Suzuki <suzukiparker5@gmail.com>
2026-08-12 13:11:55 -0600

    CH05: Add NG-ZORRO, and Angular Animations, remove chapter title as it is unneeded.

--- CityPass/src/app/app.ts
+++ CityPass/src/app/app.ts
@@ -1,15 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ChapterTitleComponent } from './chapter-title.component';
-import { ChapterTitleService } from './chapter-title.service';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ChapterTitleComponent],
+  imports: [RouterOutlet, NzLayoutModule, NzMenuModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-  private readonly chapterTitleService = inject(ChapterTitleService);
-  protected readonly title = this.chapterTitleService.title;
-}
+export class App {}
